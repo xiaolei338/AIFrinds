6a159020bb78f4c11b6e38ec14949df8af148b96
xiaolei338 <3026526845@qq.com>
2026-02-12 14:46:37 +0800

    实现后端

---
+++ backend/web/views/user/account/refresh_token.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RefreshTokenView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            if not refresh_token:
+                return Response({
+                    'result': 'refresh token不存在',
+                }, status=401)
+            refresh = RefreshToken(refresh_token)
+            if settings.SIMPLE_JWT['ROTATE_REFRESH_TOKENS']:
+                refresh.set_jti()
+                response = Response({
+                    'result': 'success',
+                    'access': str(refresh.access_token)
+                })
+                response.set_cookie(
+                    key='refresh token',
+                    value=str(refresh),
+                    httponly=True,
+                    samesite='Lax',
+                    secure=True,
+                    max_age=86400 * 7,
+                )
+                return response
+            return Response({
+                'result': 'success',
+                'access': str(refresh.access_token)
+            })
+        except:
+            return Response({
+                'result': 'refresh token error'
+            }, status=401)
