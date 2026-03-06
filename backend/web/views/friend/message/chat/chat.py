8e513c61ce7fc19f846144f7b975e530a71202d2
xiaolei338 <3026526845@qq.com>
2026-03-06 13:36:16 +0800

    添加系统提示词和多轮对话

--- backend/web/views/friend/message/chat/chat.py
+++ backend/web/views/friend/message/chat/chat.py
@@ -1,13 +1,13 @@
 import json
 
 from django.http import StreamingHttpResponse
-from langchain_core.messages import HumanMessage, BaseMessageChunk
+from langchain_core.messages import HumanMessage, BaseMessageChunk, SystemMessage
 from rest_framework.renderers import BaseRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from web.models.friend import Friend, Message
+from web.models.friend import Friend, Message, SystemPrompt
 from web.views.friend.message.chat.graph import ChatGraph
 
 
@@ -16,6 +16,26 @@ class SSERenderer(BaseRenderer):
     format = 'txt'
     def render(self, data, accepted_media_type=None, renderer_context=None):
         return data
+
+def add_system_prompt(state,friend):
+    msgs = state['messages']
+    system_prompts = SystemPrompt.objects.filter(title='回复').order_by('order_number')
+    prompt = ''
+    for sp in system_prompts:
+        prompt+=sp.prompt
+    prompt += f'\n【角色性格】\n{friend.character.profile}\n'
+    return {'messages': [SystemMessage(prompt)] + msgs}
+
+def add_recent_prompt(state,friend):
+    msgs = state['messages']
+    message_raw = list(Message.objects.filter(friend=friend).order_by('-id')[:10])
+    message_raw.reverse()
+    messages = []
+    for m in message_raw:
+        messages.append(HumanMessage(m.user_message))
+        from langchain_core.messages import AIMessage
+        messages.append(AIMessage(m.output))
+    return {'messages': msgs[:1] + messages + msgs[:-1]}
 
 class MessageChatView(APIView):
     permission_classes = [IsAuthenticated]
@@ -38,6 +58,9 @@ class MessageChatView(APIView):
         inputs = {
             'messages': [HumanMessage(message)],
         }
+        inputs = add_system_prompt(inputs, friend)
+        inputs = add_recent_prompt(inputs, friend)
+
 
         def event_stream():
             full_output = ''
